383ddd0907c15cd6c756e4d4a38697f624f3aedc
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2024-04-25 23:14:47 +0300

    Add files via upload

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -3863,7 +3863,8 @@ const Lc = Io(Tc, [["render", Fc]])
         dataForClipboard() {
             return {
                 title: "\u041F\u043E\u0432\u0456\u0434\u043E\u043C\u043B\u0435\u043D\u043D\u044F \u043F\u0440\u043E \u0432\u0438\u044F\u0432\u043B\u0435\u043D\u043D\u044F \u0446\u0456\u043B\u0456",
-                text: (this.form.time ?  this.form.time + ` : "") + (this.form.sign ? ` ${this.form.sign}
+                text: (this.form.time ? "\u0427: " + this.form.time + `
+` : "") + (this.form.sign ? `\u041F: ${this.form.sign}
 ` : "") + (this.form.nearestCity ? `\u041D.\u041F.: ${this.form.nearestCity}                
 ` : "") + (this.form.target ? `\u0426\u0456\u043B\u044C: ${this.form.target}` : "") + (this.form.target_side ? ` (${this.form.target_side})
 ` : `
@@ -3949,7 +3950,8 @@ const Lc = Io(Tc, [["render", Fc]])
                 timeZone: "Europe/Kiev",
                 hourCycle: "h23",
                 hour: "2-digit",
-                minute: "2-digit"
+                minute: "2-digit",
+                second: "2-digit"
             })
         },
         getCoordinates() {
