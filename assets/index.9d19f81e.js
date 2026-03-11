dfe4df5fce771474808d2136225108fd3b07c58a
bankir2000 <162804434+bankir2000@users.noreply.github.com>
2026-03-11 23:21:27 +0200

    Update index.9d19f81e.js

--- assets/index.9d19f81e.js
+++ assets/index.9d19f81e.js
@@ -3974,7 +3974,7 @@ const Lc = Io(Tc, [["render", Fc]])
   (this.form.other_weapon && this.form.other_weapon_ammo
     ? " Витрати БК " + this.form.other_weapon + "=" + this.form.other_weapon_ammo + "шт. "
     : "") +
-  (this.form.description ? " " + this.form.description : "") + (this.form.target_action && this.form.na_bch ? "На БЧ: " + this.form.na_bch : "")
+  (this.form.description ? " " + this.form.description : "") + (this.form.target_action && this.form.na_bch ? " На БЧ: " + this.form.na_bch : "")
             }
         },
         signErrorMessage() {
@@ -6093,6 +6093,7 @@ const Is = yc(vu);
 Is.use(Ec());
 Is.use(Pf);
 Is.mount("#app");
+
 
 
 
